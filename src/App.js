b89b5a43198b1c656ea4a282b678cffe09feacc7
afradoor <afradoor2013@gmail.com>
2020-08-19 17:12:49 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './css/components.css';
+import './css/layout.css';
+import './css/global.css';
+import './css/sample-wallpaper.css';
+
+import Mycode from './component/mycode';
+
+function App() {
+  return (
+    <div>
+
+        <Mycode/>
+        
+    </div>
+  );
+}
+
+export default App;
